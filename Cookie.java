76e165df3a3076d51e185b22e4b436a95ea97b68
Mickael Matuchak <mickael.matuchak@etudiant.univ-lille1.fr>
2019-04-25 21:22:46 +0200

    Warmup

--- Cookie.java
+++ Cookie.java
@@ -1,6 +1,7 @@
 package cookie;
 import robocode.*;
-//import java.awt.Color;
+import java.awt.Color;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
 
@@ -9,24 +10,26 @@ import robocode.*;
  */
 public class Cookie extends AdvancedRobot
 {
+	int count = 0;
+
 	/**
 	 * run: Cookie's default behavior
 	 */
 	public void run() {
-		// Initialization of the robot should be put here
+		// Couleur
+		setColors(Color.black,Color.black,Color.black); // body,gun,radar
 
-		// After trying out your robot, try uncommenting the import at the top,
-		// and the next line:
-
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		// Vitesse		
+		setMaxVelocity(Rules.MAX_VELOCITY);
 
 		// Robot main loop
 		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			count++;
+			// Se déplace si rien ne se passe
+			if (count / 100 == 0) {
+				ahead(100);
+				setTurnRightRadians(360);
+			}
 		}
 	}
 
@@ -34,23 +37,37 @@ public class Cookie extends AdvancedRobot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		fire(1);
+		// Direction vers le robot scanné
+		setTurnRightRadians(normalRelativeAngleDegrees((getHeading() - e.getHeading())));
+
+		// Tir
+		if (e.getDistance() < 200 && getEnergy() > 50) {
+			fire(Rules.MAX_BULLET_POWER);
+		} else {
+			fire(Rules.MAX_BULLET_POWER / 2);
+		}
+
+		scan();
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+	    if (getEnergy() < 75){
+	        turnRight(e.getBearing() * -1);
+	    }
+	    else {
+	        turnRight(360);
+		}
+
+        ahead(10);
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
+		back(100);
+	}
 }
